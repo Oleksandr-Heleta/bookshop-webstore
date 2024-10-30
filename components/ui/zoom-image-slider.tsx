17cd1e6a88401cf5c053180a38363bab08d96a24
Oleksandr-Heleta <sanock.geleta@gmail.com>
2024-10-30 11:12:49 +0200

    added zoom button

--- components/ui/zoom-image-slider.tsx
+++ components/ui/zoom-image-slider.tsx
@@ -1,9 +1,12 @@
 'use client';
 
 import { useKeenSlider } from 'keen-slider/react';
+import { ZoomIn, ZoomOut } from 'lucide-react';
 import Image from 'next/image';
 import React, { useRef, useState } from 'react';
 import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
+
+import IconButton from './icon-button';
 
 function Arrow(props: {
   disabled: boolean;
@@ -41,6 +44,7 @@ const ZoomImageSlider: React.FC<SliderProps> = ({ slides, startSlide }) => {
   const [loaded, setLoaded] = useState(false);
 
   const imageRef = useRef<HTMLImageElement>(null);
+  const transformRef = useRef<any>(null);
   const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
     initial: startSlide ?? 0,
     slideChanged(slider) {
@@ -49,7 +53,7 @@ const ZoomImageSlider: React.FC<SliderProps> = ({ slides, startSlide }) => {
     created() {
       setLoaded(true);
     },
-    drag: false, // Забороняємо зміну слайду перетягуванням
+    drag: false,
   });
 
   return (
@@ -57,7 +61,7 @@ const ZoomImageSlider: React.FC<SliderProps> = ({ slides, startSlide }) => {
       <div
         ref={sliderRef}
         className="keen-slider relative flex aspect-square cursor-pointer items-center rounded-md bg-white"
-        style={{ touchAction: 'none' }} // Додаємо touchAction, щоб уникнути блокування подій
+        style={{ touchAction: 'none' }}
       >
         {slides?.map((slide, idx) => (
           <div
@@ -65,6 +69,7 @@ const ZoomImageSlider: React.FC<SliderProps> = ({ slides, startSlide }) => {
             className={`keen-slider__slide relative h-full w-full aspect-square inset-0 overflow-hidden rounded-md }`}
           >
             <TransformWrapper
+              ref={(ref) => (transformRef.current = ref)}
               maxScale={5}
               onZoomStop={(ref) => {
                 if (ref.state.scale >= 5) {
@@ -74,22 +79,44 @@ const ZoomImageSlider: React.FC<SliderProps> = ({ slides, startSlide }) => {
               pinch={{ disabled: false, step: 0.5 }}
               doubleClick={{ disabled: false, step: 0.5 }}
             >
-              <TransformComponent
-                contentClass="relative w-full h-full cursor-zoom-in"
-                contentStyle={{ touchAction: 'manipulation' }}
-              >
-                <Image
-                  ref={imageRef}
-                  src={slide}
-                  alt={slide}
-                  width={1000}
-                  height={1000}
-                  className={'object-contain object-center cursor-zoom-in '}
-                  priority={idx === 0}
-                  onLoad={() => instanceRef.current?.update()}
-                  style={{ touchAction: 'manipulation' }}
-                />
-              </TransformComponent>
+              {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                <>
+                  <div className="absolute z-10 bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+                    <IconButton
+                      aria-label="Збільшити"
+                      onClick={() => zoomIn()}
+                      className=""
+                      icon={
+                        <ZoomIn size={20} className="text-white bg-inherit " />
+                      }
+                    />{' '}
+                    <IconButton
+                      aria-label="Збільшити"
+                      onClick={() => zoomOut()}
+                      className=""
+                      icon={
+                        <ZoomOut size={20} className="text-white bg-inherit " />
+                      }
+                    />
+                  </div>
+                  <TransformComponent
+                    contentClass="relative w-full h-full cursor-zoom-in"
+                    contentStyle={{ touchAction: 'manipulation' }}
+                  >
+                    <Image
+                      ref={imageRef}
+                      src={slide}
+                      alt={slide}
+                      width={1000}
+                      height={1000}
+                      className={'object-contain object-center cursor-zoom-in '}
+                      priority={idx === 0}
+                      onLoad={() => instanceRef.current?.update()}
+                      style={{ touchAction: 'manipulation' }}
+                    />
+                  </TransformComponent>
+                </>
+              )}
             </TransformWrapper>
           </div>
         ))}
